6021e55756ee4892f8ca3a7149481a23844e99e2
marson parulian <marson.parulian@gmail.com>
2022-05-20 19:03:34 +1000

    relay response from event cinema

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var fetch = require('cross-fetch');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/now-showing', async (req, res) => {
+  const url = 'https://www.eventcinemas.com.au/Movies/GetNowShowing/';
+  const received = await fetch(url).catch(e => console.error(e));
+  const json = await received.json();
+  res.json(json);
 });
 
 module.exports = router;
