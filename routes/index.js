4a6bafda10367033883b1420c070c90a561ac05d
marson parulian <marson.parulian@gmail.com>
2022-05-25 21:46:10 +1000

    Add end point to relay 'comming soon'

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,17 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Relay 'now showing' movie list
 router.get('/now-showing', async (req, res) => {
   const url = 'https://www.eventcinemas.com.au/Movies/GetNowShowing/';
+  const received = await fetch(url).catch(e => console.error(e));
+  const json = await received.json();
+  res.json(json);
+});
+
+// Relay 'coming soon' movie list
+router.get('/coming-soon', async (req, res) => {
+  const url = 'https://www.eventcinemas.com.au/Movies/GetComingSoon/';
   const received = await fetch(url).catch(e => console.error(e));
   const json = await received.json();
   res.json(json);
